713e0eff2aea904ab80e8de8055825cf65982e1f
santiago Álvarez <santiagoaas10@gmail.com>
2024-01-03 23:29:43 -0500

    clase#13, manejo de los console

---
+++ manejoConsola/consola.js
@@ -0,0 +1,53 @@
+/* var tabla = [
+    {
+        a:1,
+        b:'Z'
+    },
+    {
+        a:2,
+        b:'OTRA LETRA'
+    },
+
+]
+
+console.log(tabla)
+console.table(tabla)//lindo, lindo */
+
+
+
+/* //indenta los console.log que estén dentro
+console.log('conversacion')
+console.group('conver')
+console.log('hola')
+console.log('blalalallaa')
+console.log('adios')
+console.groupEnd('conver')
+console.log('otras cosas de otra función')
+ */
+
+
+/* function funcion1(){
+    console.group('funcion1')
+    console.log('esto es de la funcion 1')
+    console.log('esto es de la funcion 1')
+    console.log('esto es de la funcion 1')
+    funcion2()
+    console.groupEnd('funcion1')
+}
+
+function funcion2(){
+    console.group('funcion2')
+    console.log('ahora la fnc2')
+    console.log('ahora la fnc2')
+    console.log('ahora la fnc2')
+    console.groupEnd('funcion2')
+}
+
+console.log("empezamos")
+funcion1() */
+
+console.count('veces')
+console.count('veces')
+console.count('veces')
+console.countReset('veces')
+console.count('veces')
