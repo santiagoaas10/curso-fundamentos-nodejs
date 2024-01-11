6ea9b74ad9e25041bfe9c3e4ba3bdbf4bbd4d93c
santiago Álvarez <santiagoaas10@gmail.com>
2024-01-10 21:02:59 -0500

    clase 21 creación y uso de módulos propios, clase trabajada en la carpeta llamada modulos y archivos construyemodulos y usarmodulos

---
+++ modulos/construyemodulo.js
@@ -0,0 +1,9 @@
+
+function saludar(){
+    console.log('hola mundo')
+}
+
+module.exports ={ 
+    saludar,
+    prop1:"hola quetal"
+}
---
+++ modulos/usarmodulo.js
@@ -0,0 +1,7 @@
+//traer nuestro construyemodulo y ejecutar una función saludar del construyemódulo
+const modulo = require('./construyemodulo');
+console.log(modulo)
+
+
+console.log(modulo.prop1);
+modulo.saludar()
